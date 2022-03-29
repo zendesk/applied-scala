f08d9f1d03afd1329848612f3a1bfb5b2b33f470
Lachlan O'Dea <lodea@zendesk.com>
2022-03-29 15:24:44 +1100

    Circe solutions

--- src/main/scala/com/reagroup/exercises/circe/CirceExercises.scala
+++ src/main/scala/com/reagroup/exercises/circe/CirceExercises.scala
@@ -29,7 +29,7 @@ object CirceExercises {
     * Why is the return type an `Either`?
     */
   def strToJson(str: String): Either[ParsingFailure, Json] = {
-    ???
+    parser.parse(str)
   }
 
   /**
@@ -62,6 +62,8 @@ object CirceExercises {
       * Make `personEncoder` an `implicit` to avoid having to pass the `Encoder` instance
       * into `asJson` explicitly.
       *
+      * How to write Circe custom Codecs: https://circe.github.io/circe/codecs/custom-codecs.html
+      *
       * Bonus content (if you have time):
       *
       * You can read the code below as "Person is an instance of the Encoder type class"
@@ -72,7 +74,7 @@ object CirceExercises {
       * - https://www.parsonsmatt.org/2017/01/07/how_do_type_classes_differ_from_interfaces.html
       */
     implicit val personEncoder: Encoder[Person] = Encoder { (p: Person) =>
-      ???
+      Json.obj("name" -> p.name.asJson, "age" -> p.age.asJson)
     }
 
     /**
@@ -104,7 +106,7 @@ object CirceExercises {
     * Use `differentPersonEncoder` explicitly to encode the person
     */
   def encodePersonDifferently(person: Person): Json = {
-    person.asJson(???)
+    person.asJson(Person.differentPersonEncoder)
   }
 
   /**
@@ -122,7 +124,7 @@ object CirceExercises {
   def encodePersonSemiAuto(person: Person): Json = {
     import io.circe.generic.semiauto._
 
-    implicit val personEncoder: Encoder[Person] = ???
+    implicit val personEncoder: Encoder[Person] = deriveEncoder
     person.asJson
   }
 
@@ -158,9 +160,10 @@ object CirceExercises {
   def decodePerson(json: Json): Either[DecodingFailure, Person] = {
     import cats.implicits._
 
-    implicit val personDecoder: Decoder[Person] = new Decoder[Person] {
-      override def apply(cursor: HCursor): Result[Person] = ???
-    }
+    implicit val personDecoder: Decoder[Person] = (cursor: HCursor) => for {
+      name <- cursor.downField("name").as[String]
+      age <- cursor.downField("age").as[Int]
+    } yield Person(name, age)
     // note: a lot of boilerplate can be removed. Try pressing alt-enter with your
     // cursor over "new Decoder[Person]" above. This works because Decoder is a trait with
     // a single abstract method.
@@ -177,7 +180,7 @@ object CirceExercises {
   def decodePersonSemiAuto(json: Json): Either[DecodingFailure, Person] = {
     import io.circe.generic.semiauto._
 
-    implicit val personDecoder: Decoder[Person] = ???
+    implicit val personDecoder: Decoder[Person] = deriveDecoder
 
     json.as[Person]
   }
@@ -190,9 +193,9 @@ object CirceExercises {
   def strToPerson(str: String): Either[Error, Person] = {
     import io.circe.generic.semiauto._
 
-    implicit val personDecoder: Decoder[Person] = ???
+    implicit val personDecoder: Decoder[Person] = deriveDecoder
 
-    ???
+    parser.decode(str)
   }
 
 }
